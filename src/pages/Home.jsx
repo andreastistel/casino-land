0bf084d02b433d33705cef5f9d1f52b731a4cc6f
Andreas <andreas.tistel@highsoft.com>
2026-02-25 15:29:10 +0100

    feat: add highsoft-ui library and integrate Header component in Home page
    - Added highsoft-ui dependency to package.json
    - Imported highsoft-ui CSS in index.js
    - Integrated Header component from highsoft-ui in Home.jsx with navigation items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import { Header } from 'highsoft-ui';
 import PackedBubbleChart from '../components/PackedBubbleChart';
+
+const navItems = [
+  { title: 'Home', url: '/' },
+  { title: 'Charts', url: '/charts' },
+  { title: 'Games', url: '/games' },
+];
 
 function Home() {
   return (
     <div>
+      <Header subItems={navItems} pathname="/" />
       <PackedBubbleChart />
     </div>
   );
